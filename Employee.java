95cdc961209ad1528ab03f4e8d43f67ef6d40be3
harshithadega <37622329+harshithadega@users.noreply.github.com>
2020-01-08 21:28:33 -0800

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,37 @@
+package hbn;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	@Id
+	private int empcode;
+    @Column(length=25)
+    private String empname;
+    @Column
+    private double salary;
+	public int getEmpcode() {
+		return empcode;
+	}
+	public void setEmpcode(int empcode) {
+		this.empcode = empcode;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+    
+ 
+}
